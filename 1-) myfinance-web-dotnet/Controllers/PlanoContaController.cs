6e70f36b8b25a153c42b0869bafb77d2885d04c3
Davi Henrique dos Santos Silva <davi.henrique.silva597@gmail.com>
2024-03-10 01:22:28 -0300

    aula: formulario

--- 1-) myfinance-web-dotnet/Controllers/PlanoContaController.cs
+++ 1-) myfinance-web-dotnet/Controllers/PlanoContaController.cs
@@ -1,6 +1,6 @@
-using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using myfinance_web_dotnet.Models;
+using myfinance_web_dotnet_domain.Entities;
 using nyfinance_web_dotnet_service.Interfaces;
 
 namespace myfinance_web_dotnet.Controllers;
@@ -37,6 +37,50 @@ public class PlanoContaController : Controller
         }
         ViewBag.ListaPlanoConta = listaPlanoContaModel;
         return View();
+    }
+
+    [HttpGet]
+    [Route("Cadastrar")]
+    [Route("Cadastrar/{Id}")]
+    public IActionResult Cadastrar(int? Id)
+    {
+        if(Id is null)
+          return View();
+
+        var planoConta = _planoContaService.RetornarRegistro((int)Id);
+        var planoContaModel = new PlanoContaModel{
+            Id = planoConta.Id,
+            Descricao = planoConta.Descricao,
+            Tipo = planoConta.Tipo
+        };
+
+        return View(planoContaModel);
+    }
+
+    [HttpPost]
+    [Route("Cadastrar")]
+    [Route("Cadastrar/{Id}")]
+    public IActionResult Cadastrar(PlanoContaModel model)
+    {
+       var planoconta = new PlanoConta
+        {
+            Id = model.Id,
+            Descricao = model.Descricao,
+            Tipo = model.Tipo
+        };
+
+        _planoContaService.Cadastrar(planoconta);
+
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    [Route("Excluir/{Id}")]
+    public IActionResult Exlcuir(int? Id)
+    {
+        _planoContaService.Excluir((int)Id);
+
+        return RedirectToAction("Index");
     }
 
     [ResponseCache(Duration = 0, Location =ResponseCacheLocation.None, NoStore =true)]
